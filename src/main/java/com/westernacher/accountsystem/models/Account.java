c4b7337e4f1547894014b1b63853a0aa40a17f84
Ognyan Ignatov <klan_nedoklan@mail.bg>
2020-05-15 22:36:49 +0300

    finished crud db

--- src/main/java/com/westernacher/accountsystem/models/Account.java
+++ src/main/java/com/westernacher/accountsystem/models/Account.java
@@ -1,20 +1,67 @@
 package com.westernacher.accountsystem.models;
 
-import lombok.Builder;
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Document(collection = "accounts")
-@Data
-@Builder
-public class Account {
+public class Account implements Serializable {
     @Id
-    String id;
-    String firstName;
-    String lastName;
-    String email;
-    LocalDate dateOfBirth;
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+
+    public Account() {
+    }
+
+    public Account(String firstName, String lastName, String email, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
---
+++ src/main/java/com/westernacher/accountsystem/utils/ExceptionUtils.java
@@ -0,0 +1,9 @@
+package com.westernacher.accountsystem.utils;
+
+public final class ExceptionUtils {
+
+    public static final String ACCOUNT_NOT_FOUND = "Cannot find account with ID: ";
+
+    private ExceptionUtils() {
+    }
+}
