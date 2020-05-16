67c3af9eb0c1ad9f9bee2324fd51353cbb72666f
Ognyan Ignatov <klan_nedoklan@mail.bg>
2020-05-16 13:16:22 +0300

    updated pom and added standalone packaging

---
+++ src/main/resources/static/precache-manifest.57ed7703b9a0c0ad6d1afb35657621df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6945625fd75a109726f86202d906cc2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199d240d9acccb308a2b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ed8f8d44b6e995959e5",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "199d240d9acccb308a2b",
+    "url": "/static/js/2.c9402bbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9402bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed8f8d44b6e995959e5",
+    "url": "/static/js/main.c1b730d5.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
