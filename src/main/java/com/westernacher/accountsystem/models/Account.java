3ce352d472e7b44c48d1f172aa04f4b44785bf8b
Ognyan Ignatov <klan_nedoklan@mail.bg>
2020-05-17 14:12:01 +0300

    added mockmvc test

--- src/main/java/com/westernacher/accountsystem/models/Account.java
+++ src/main/java/com/westernacher/accountsystem/models/Account.java
@@ -1,5 +1,6 @@
 package com.westernacher.accountsystem.models;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -7,6 +8,11 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 @Document(collection = "accounts")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account implements Serializable {
     @Id
     private String id;
@@ -15,53 +21,10 @@ public class Account implements Serializable {
     private String email;
     private LocalDate dateOfBirth;
 
-    public Account() {
-    }
-
     public Account(String firstName, String lastName, String email, LocalDate dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 }
